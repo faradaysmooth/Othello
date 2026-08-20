dd4264decad7bbabf75f3ffbaadf10f013ff2332
faradaysmooth <03.faraday.smooth@icloud.com>
2026-08-19 18:23:35 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'othello-v1';
+const C = 'othello-v2';
 self.addEventListener('install', e => e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => n === C ? null : caches.delete(n)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))));
